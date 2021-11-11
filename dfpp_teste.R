592d3ea979fa2e598c9740d847f2b5ecbe71c536
alenc09 <alenc09@protonmail.com>
2021-11-11 17:00:16 -0300

    finalizei as anÃálises para caatinga e comecei a preparar a apresentação

--- dfpp_teste.R
+++ dfpp_teste.R
@@ -7,6 +7,7 @@ library(geobr)
 library(dplyr)
 library(here)
 library(sf)
+library(ggplot2)
 
 #data----
 read_biomes(simplified = F) %>%
@@ -20,12 +21,12 @@ raster(x = here("data/bra_ppp_2020_UNadj_constrained.tif")) -> br_pop
 raster(x = here("data/pop_caat_polybr_1000.tif")) -> pop_caat_polybr_1000
 
 #data manipulation----
-## Caatinga shape
+## Caatinga shape----
 st_transform(caat_shp, crs = 4326) -> caat_shp_wgs84 #coordinate system change to wgs84
 as_Spatial(caat_shp_wgs84) -> caat_shp_wgs84_sp #object class change to be used in crop and mask functions
 st_transform(caat_shp, crs = 5880) ->caat_shp_polybr
 
-##population data
+##population data----
 raster::crop(x = br_pop, y = caat_shp_wgs84_sp) -> br_pop_crop
 raster::mask(x = br_pop_crop, mask = caat_shp_wgs84_sp) -> pop_caat_wgs84 #filtering only population in Caatinga
 aggregate(x = pop_caat_wgs84, fact = 10, fun = sum) ->pop_caat_wgs84_1000
@@ -33,7 +34,14 @@ aggregate(x = pop_caat_wgs84, fact = 10, fun = sum) ->pop_caat_wgs84_1000
 raster::projectRaster(pop_caat_wgs84_1000, crs = proj_polyBR, method = "bilinear")-> pop_caat_polybr_1000
 pop_caat_polybr_1000[is.na(pop_caat_polybr_1000)] <- 0 #change "no data" to zero population
 pop_caat_polybr_1000 -> pop_caat_rural_polybr
-pop_caat_rural_polybr[pop_caat_rural_polybr > 130] <- 0 #excluding urban areas
+pop_caat_rural_polybr[pop_caat_rural_polybr < 130] <- 0 #excluding urban areas
+
+###data to points----
+raster::rasterToPoints(pop_caat_polybr_1000) %>% 
+  tibble::as_tibble() ->pop_caat_tibble
+
+raster::rasterToPoints(pop_caat_rural_polybr) %>% 
+  tibble::as_tibble() ->pop_caat_rural_tibble
 
 ##plots----
 df_plots %>% #filtering only plots in Caatinga
@@ -82,14 +90,76 @@ raster::extract(x = pop_caat_rural_polybr,
 
 
 ##all caatinga population----
+raster::extract(x = pop_caat_polybr_1000,
+                y = buff_1km_union,
+                fun = sum,
+                na.rm = FALSE) -> people_1km
+raster::extract(x = pop_caat_polybr_1000,
+                y = buff_5km_union,
+                fun = sum,
+                na.rm = FALSE) -> people_5km
+raster::extract(x = pop_caat_polybr_1000,
+                y = buff_10km_union,
+                fun = sum,
+                na.rm = FALSE) -> people_10km
+## Results ----
+c("rural only", "total")-> Population
+c(people_rural_1km, people_1km) -> pop1km
+c(people_rural_5km, people_5km) -> pop5km
+c(people_rural_10km, people_10km) -> pop10km
+cbind(Population, pop1km, pop5km, pop10km)-> df_results
 
-#data visualization----
+
+# data visualization ----
+
+#plot(br_pop)
+plot(pop_caat_wgs84)
 plot(pop_caat_polybr_1000)
 plot(pop_caat_rural_polybr)
-plot(pop_caat_polybr_1000 > 130)
+plot(pop_caat_polybr_1000 < 130 & pop_caat_polybr_1000 > 0)
+#plot(buff_10km_union)
+#plot(caat_shp_polybr$geom)
+#plot(plot_caat_polybr, add = T)
 
-plot(caat_shp_polybr$geom)
-plot(plot_caat_polybr, add = T)
+ggplot() +
+  geom_raster(data = pop_caat_rural_tibble, aes(x=x, y=y, fill=pop_caat_polybr_1000))+
+  geom_sf(data = caat_shp_polybr, fill= NA, colour = "black", size = .3) +
+  geom_sf(data = plot_caat_polybr, size =0.1) +
+  geom_sf(data=st_as_sf(buff_1km_union), fill = NA, col = "red") +
+  geom_sf(data=st_as_sf(buff_5km_union), fill = NA, col = "red") +
+  geom_sf(data=st_as_sf(buff_10km_union), fill = NA, col = "red") +
+  scale_fill_viridis_c(name = "Rural population in Caatinga") +
+  theme(panel.background = element_blank(),
+        axis.title = element_blank()
+        ) -> caat_pop_rural.fig
+
+ggplot() +
+  geom_raster(data = pop_caat_rural_tibble, aes(x=x, y=y, fill=pop_caat_polybr_1000))+
+  geom_sf(data = caat_shp_polybr, fill= NA, colour = "black", size = .3) +
+  geom_sf(data = plot_caat_polybr, size =0.1) +
+  geom_sf(data=st_as_sf(buff_1km_union), fill = NA, col = "red") +
+  geom_sf(data=st_as_sf(buff_5km_union), fill = NA, col = "red") +
+  geom_sf(data=st_as_sf(buff_10km_union), fill = NA, col = "red") +
+  coord_cartesian(xlim = c(6500000,7000000), ylim = c(9000000,9500000))+
+  coord_sf(xlim = c(6800000,7000000), ylim = c(9000000,9200000))+
+  scale_fill_viridis_c(name = "Rural population in Caatinga") +
+  theme(panel.background = element_blank(),
+        axis.title = element_blank()
+  )
+
+ggplot() +
+  geom_raster(data = pop_caat_tibble, aes(x=x, y=y, fill=pop_caat_polybr_1000))+
+  geom_sf(data = caat_shp_polybr, fill= NA, colour = "black", size = .3) +
+  geom_sf(data = plot_caat_polybr, size =0.1) +
+  geom_sf(data=st_as_sf(buff_1km_union), fill = NA, col = "red") +
+  geom_sf(data=st_as_sf(buff_5km_union), fill = NA, col = "red") +
+  geom_sf(data=st_as_sf(buff_10km_union), fill = NA, col = "red") +
+  scale_fill_viridis_c()+
+  theme(panel.background = element_blank(),
+        axis.title = element_blank()
+        ) -> caat_pop.fig
+
+
 
 #data export----
 writeRaster(x = pop_caat_polybr_1000, filename = here("data/pop_caat_polybr_1000.tif"))
