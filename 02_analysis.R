ed75d7ee2ef8bf31d64ec4692021770fd18ea0c6
Lucas Alencar <alencar.lucasc@gmail.com>
2022-03-23 10:39:29 -0300

    Update 02_analysis.R

--- 02_analysis.R
+++ 02_analysis.R
@@ -28,3 +28,92 @@ landscapemetrics::sample_lsm(landscape = mapbiomas_caat,
                              level = c("class", "class", "landscape"),
                              metric = c("ca","pland", "shdi")
                              )-> buff_lsm
+
+##data on people and households inside buffers----
+data_pop_vars_forest%>%
+  tibble()%>%
+  select(-geometry)%>%
+  group_by(id_buff,
+           CD_GEOC
+           )%>%
+  summarise(pop_buff = sum(POP),
+            dom_buff = sum(DOM_OCU),
+            dom_sc = sum(V1),
+            dom_expov = sum(V19),
+            dom_Senerg = sum(V11),
+            dom_cist = sum(V6),
+            pop_analf = sum(V12),
+            pop_sc = sum(V13),
+  )%>%
+  mutate(prop_dom_expov = dom_expov/dom_sc,
+         dom_expov_buff = dom_buff * prop_dom_expov,
+         prop_dom_Senerg = dom_Senerg/dom_sc,
+         dom_Senerg_buff = dom_buff * prop_dom_Senerg,
+         prop_dom_Scist = 1-(dom_cist/dom_sc),
+         dom_Scist_buff = dom_buff * prop_dom_Scist,
+         prop_pop_analf = pop_analf/pop_sc,
+         pop_analf_buff = pop_buff * prop_pop_analf
+  )%>%
+  glimpse -> result_5km_forest
+
+result_5km_forest%>%
+  ungroup()%>%
+  summarise(dom_total_expov = sum(dom_expov_buff, na.rm = T),
+            dom_total_Senerg = sum(dom_Senerg_buff, na.rm = T),
+            dom_total_Scist = sum(dom_Scist_buff, na.rm = T),
+            pop_total_analf = sum(pop_analf_buff, na.rm = T)
+  )%>%
+  glimpse -> total_5km_forest
+
+###non-forest----
+data_pop_vars_Nforest%>%
+  tibble()%>%
+  select(-geometry)%>%
+  group_by(id_buff,
+           CD_GEOC
+           )%>%
+  summarise(pop_buff = sum(POP),
+            dom_buff = sum(DOM_OCU),
+            dom_sc = sum(V1),
+            dom_expov = sum(V19),
+            dom_Senerg = sum(V11),
+            dom_cist = sum(V6),
+            pop_analf = sum(V12),
+            pop_sc = sum(V13),
+            )%>%
+  mutate(prop_dom_expov = dom_expov/dom_sc,
+         dom_expov_buff = dom_buff * prop_dom_expov,
+         prop_dom_Senerg = dom_Senerg/dom_sc,
+         dom_Senerg_buff = dom_buff * prop_dom_Senerg,
+         prop_dom_Scist = 1-(dom_cist/dom_sc),
+         dom_Scist_buff = dom_buff * prop_dom_Scist,
+         prop_pop_analf = pop_analf/pop_sc,
+         pop_analf_buff = pop_buff * prop_pop_analf
+         )%>%
+  glimpse -> result_5km_Nforest
+
+result_5km_Nforest%>%
+  ungroup()%>%
+  summarise(dom_total_expov = sum(dom_expov_buff, na.rm = T),
+            dom_total_Senerg = sum(dom_Senerg_buff, na.rm = T),
+            dom_total_Scist = sum(dom_Scist_buff, na.rm = T),
+            pop_total_analf = sum(pop_analf_buff, na.rm = T)
+  )%>%
+  glimpse -> total_5km_Nforest
+
+result_5km_forest%>%
+  bind_rows(result_5km_Nforest)%>%
+  summarise(pop = sum(pop_buff),
+            dom = sum(dom_buff),
+            dom_expov = sum(dom_expov_buff),
+            dom_Senerg = sum(dom_Senerg_buff),
+            dom_Scist = sum(dom_Scist_buff),
+            pop_analf = sum(pop_analf_buff)
+            )%>%
+  mutate(prop_dom_expov = dom_expov/dom,
+         prop_dom_Senerg = dom_Senerg/dom,
+         prop_dom_Scist = dom_Scist/dom,
+         prop_pop_analf = pop_analf/pop,
+         buff_class = if_else(id_buff <= 1605, true = "forest", false = "non-forest")
+         ) %>% 
+  glimpse -> table_analysis2
